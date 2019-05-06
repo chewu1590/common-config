f90b27a7fbd7e499950520abc782a511e39f9bc2
woochen <cwc0912@foxmail.com>
2019-05-06 16:24:44 +0800

    +++

--- common_config/src/main/java/cn/woochen/common_config/base/BaseFragment.kt
+++ common_config/src/main/java/cn/woochen/common_config/base/BaseFragment.kt
@@ -2,10 +2,10 @@ package cn.woochen.common_config.base
 
 
 import android.app.Activity
-import android.app.Fragment
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
