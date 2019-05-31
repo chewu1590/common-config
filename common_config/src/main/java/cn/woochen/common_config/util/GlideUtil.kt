fe3d67edea91a343de167deae9d869611939aa4d
woochen <cwc0912@foxmail.com>
2019-05-31 13:41:37 +0800

    new version 1.0.1

--- common_config/src/main/java/cn/woochen/common_config/util/GlideUtil.kt
+++ common_config/src/main/java/cn/woochen/common_config/util/GlideUtil.kt
@@ -1,7 +1,10 @@
 package cn.woochen.common_config.util
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.support.annotation.DrawableRes
 import android.widget.ImageView
+import cn.woochen.common_config.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -20,9 +23,26 @@ open class GlideUtil {
         /**
          * 加载普通图片
          */
+        @SuppressLint("CheckResult")
         fun load(context: Context, url: Any?, imageView: ImageView) {
+            val requestOptions = RequestOptions()
+            requestOptions.placeholder(R.drawable.ic_default_pic).error(R.drawable.ic_default_pic)
             Glide.with(context)
                 .load(url)
+                .apply(requestOptions)
+                .into(imageView)
+        }
+
+        /**
+         * 加载普通图片
+         */
+        @SuppressLint("CheckResult")
+        fun load(context: Context, url: Any?, imageView: ImageView, @DrawableRes  placeholderId:Int, @DrawableRes  errorId:Int) {
+            val requestOptions = RequestOptions()
+            requestOptions.placeholder(placeholderId).error(errorId)
+            Glide.with(context)
+                .load(url)
+                .apply(requestOptions)
                 .into(imageView)
         }
 
@@ -38,12 +58,40 @@ open class GlideUtil {
         }
 
         /**
+         * 加载圆型图片
+         */
+        @SuppressLint("CheckResult")
+        fun loadCircle(context: Context, url: Any?, imageView: ImageView, @DrawableRes  placeholderId:Int, @DrawableRes  errorId:Int) {
+            val requestOptions = RequestOptions.circleCropTransform().diskCacheStrategy(DiskCacheStrategy.ALL)
+            requestOptions.placeholder(placeholderId).error(errorId)
+            Glide.with(context)
+                .load(url)
+                .apply(requestOptions)
+                .into(imageView)
+        }
+
+        /**
          * 加载圆角图片
          * @param angleValue 弧度(dp)
          */
         fun loadRec(context: Context, url: Any?, imageView: ImageView, angleValue: Float) {
             val roundedCorners = RoundedCorners(UiUtils.dp2px(context, angleValue))
             val options = RequestOptions.bitmapTransform(roundedCorners)
+            Glide.with(context)
+                .load(url)
+                .apply(options)
+                .into(imageView)
+        }
+
+        /**
+         * 加载圆角图片
+         * @param angleValue 弧度(dp)
+         */
+        @SuppressLint("CheckResult")
+        fun loadRec(context: Context, url: Any?, imageView: ImageView, angleValue: Float, @DrawableRes  placeholderId:Int, @DrawableRes  errorId:Int) {
+            val roundedCorners = RoundedCorners(UiUtils.dp2px(context, angleValue))
+            val options = RequestOptions.bitmapTransform(roundedCorners)
+            options.placeholder(placeholderId).error(errorId)
             Glide.with(context)
                 .load(url)
                 .apply(options)
