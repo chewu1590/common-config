1733a1979210de74d1cdc5601c2f2b25b306a0c0
woochen <cwc0912@foxmail.com>
2019-06-14 13:28:36 +0800

    new version 1.0.2

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ Add it in your root build.gradle at the end of repositories:
 Step 2. Add the dependency
 
 	dependencies {
-	        implementation 'com.github.chewu1590:common-config:1.0.0'
+	        implementation 'com.github.chewu1590:common-config:1.0.2'
 	}
--- common_config/src/main/java/cn/woochen/common_config/util/EncryptUtil.kt
+++ common_config/src/main/java/cn/woochen/common_config/util/EncryptUtil.kt
@@ -1,6 +1,10 @@
 package cn.woochen.common_config.util
 
+import android.text.TextUtils
+import java.io.InputStream
 import java.security.MessageDigest
+import java.security.PrivateKey
+import java.security.PublicKey
 
 
 /**
@@ -43,5 +47,52 @@ object EncryptUtil {
         val d2 = n % 16
         return hexDigits[d1] + hexDigits[d2]
     }
+
+    //    ------------------<rsa start>--------------------
+
+
+    /**
+     * 加载私钥
+     */
+    fun loadPrivateKey(inPrivate: InputStream) = RsaUtil.loadPrivateKey(inPrivate)
+
+    /**
+     * 加载私钥
+     * 加载时使用的是PKCS8EncodedKeySpec（PKCS#8编码的Key指令）。
+     */
+    fun loadPrivateKey(privateKeyStr: String) = RsaUtil.loadPrivateKey(privateKeyStr)
+
+
+    /**
+     * 加载公钥
+     */
+    fun loadPublicKey(inPub: InputStream) = RsaUtil.loadPublicKey(inPub)
+
+    /**
+     * 加载公钥
+     */
+    fun loadPublicKey(publicKeyStr: String) = RsaUtil.loadPublicKey(publicKeyStr)
+
+    /**
+     *私钥解密
+     */
+    fun decryptVideoByRsa(source: String?, privateKey: PrivateKey): String {
+        if (TextUtils.isEmpty(source)) return ""
+        val decryptByte = RsaUtil.decryptData(Base64Utils.decode(source), privateKey)
+        return String(decryptByte)
+    }
+
+    /**
+     *公钥加密
+     */
+    fun encryptDocByRsa(source: String?, publicKey: PublicKey): String {
+        if (TextUtils.isEmpty(source)) return ""
+        val encryptByte = RsaUtil.encryptData(source?.toByteArray(), publicKey)
+        // 为了方便观察吧加密后的数据用base64加密转一下，要不然看起来是乱码,所以解密是也是要用Base64先转换
+        return Base64Utils.encode(encryptByte)
+    }
+
+
+//    ------------------<rsa end>--------------------
 
 }
